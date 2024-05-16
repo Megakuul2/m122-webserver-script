69f6fc7253274beab05be8b459cd68e4e39f6bc6
vmadmin <vmadmin@vmLP1.smartlearn.lan>
2024-05-16 23:39:09 +0200

    Benutzereingabe implementiert

--- setup-webserver.sh
+++ setup-webserver.sh
@@ -1,1 +1,20 @@
-#
+#!/bin/bash
+
+# Initiale Konsolenausgabe 
+echo "Webserver Setup - Noah Berclaz"
+
+# Benutzereingabe einlesen
+read input
+
+
+if [ "NGINX" = "$input" ]; then
+	echo "NGINX"
+elif [ "Apache2" = "$input" ]; then
+	echo "Apache2"
+else
+	echo "Syntax Fehler"
+
+fi
+
+
+
